af1527b72edd70b773c61d7e6348f9ec46beff97
André de Jager <andrethehunter@gmail.com>
2019-08-15 11:03:45 +1000

    Update package files. Change show to correctly escape whitespace

--- .config/fish/functions/show.fish
+++ .config/fish/functions/show.fish
@@ -1,6 +1,8 @@
+# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.1wboJj/show.fish @ line 2
 function show
+	set -l cmd (string escape -- $argv)
 	set_color yellow
-	echo $argv
+	echo "$cmd"
 	set_color normal
-	eval $argv
+	eval "$cmd"
 end
--- Gemfile
+++ Gemfile
@@ -1,4 +1,2 @@
 source 'https://rubygems.org'
-gem "mdless", "~> 0.0.9"
-
 gem "jekyll", "~> 3.8"
