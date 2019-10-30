e111562b27e1a41f27d21767254efe6972ad41e6
André de Jager <andrethehunter@gmail.com>
2019-10-30 12:36:49 +1000

    Change la to --all

--- .config/fish/functions/la.fish
+++ .config/fish/functions/la.fish
@@ -1,3 +1,4 @@
+# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.teWuwP/la.fish @ line 2
 function la
-	ls -lv --almost-all --color --group-directories-first --human-readable --kibibytes --no-group --tabsize=$TAB_WIDTH --time-style=long-iso $argv
+	ls -lv --all --color --group-directories-first --human-readable --kibibytes --no-group --tabsize=$TAB_WIDTH --time-style=long-iso $argv
 end
