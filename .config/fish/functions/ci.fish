daa09721088a063affcd65a3648f9246b5d03a2e
André de Jager <andrethehunter@gmail.com>
2019-12-11 09:26:30 +1000

    Update ci function. Update .ideavimrc

--- .config/fish/functions/ci.fish
+++ .config/fish/functions/ci.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.lTqf86/ci.fish @ line 2
 function ci
 	if command git is-git
 		cljog circle-ci r --watch 5 --notify 1 --project (basename (pwd)) $argv;
