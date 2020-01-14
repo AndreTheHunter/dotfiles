02486101ce287604e4ee74956487ade7fe201e1f
André de Jager <andrethehunter@gmail.com>
2020-01-14 15:43:47 +1000

    Reduce ci function refresh to 10

--- .config/fish/functions/ci.fish
+++ .config/fish/functions/ci.fish
@@ -1,7 +1,7 @@
 function ci
 	if command git is-git
-		show cljog circle-ci recent --watch 5 --notify 1 --project (basename (pwd)) $argv;
+		show cljog circle-ci recent --watch 10 --notify 1 --project (basename (pwd)) $argv;
 	else
-		show cljog circle-ci recent --watch 5 --notify 1 $argv;
+		show cljog circle-ci recent --watch 10 --notify 1 $argv;
 	end
 end
