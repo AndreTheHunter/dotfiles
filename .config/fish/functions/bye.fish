e1b46239a25e14f3958ea6a91a7f64fc3139b854
André de Jager <andrethehunter@gmail.com>
2020-10-02 14:58:06 +1000

    Update

--- .config/fish/functions/bye.fish
+++ .config/fish/functions/bye.fish
@@ -1,12 +1,9 @@
-# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.wTlp4u/bye.fish @ line 2
+# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.8gBNIi/bye.fish @ line 2
 function bye
 	if git is-git
 		show cdg
 		and show git co .
-		and show git checkout master
-		and show git fetch
-		and show git rebase
-		and show git co .
+		and show git cor master
 		and show git prune-merged
 	end
 	and exit $status
