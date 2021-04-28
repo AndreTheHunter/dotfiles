b84f707dcc42d26c8095d0207ef877000164e7db
André de Jager <andrethehunter@gmail.com>
2021-04-28 17:44:19 +1000

    Update

--- .config/fish/functions/ide.fish
+++ .config/fish/functions/ide.fish
@@ -1,8 +1,8 @@
-# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.kIC9sK/ide.fish @ line 2
+# Defined in /var/folders/xt/l66l0pyj6n3gzm89h072z8_m0000gn/T//fish.cNEmdi/ide.fish @ line 2
 function ide
 	set -l args
 	set -l cmd
-	if test -d '.vscode'
+	if test -d '.vscode';or test -e (basename (pwd)).csproj
 		set cmd 'code'
 	else
 		set cmd 'idea'
