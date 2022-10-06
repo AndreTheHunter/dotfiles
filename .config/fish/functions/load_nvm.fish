26b0be4c443f4203772331e1a55d0f500b75b844
André de Jager <AndreTheHunter@gmail.com>
2022-10-06 13:41:58 +1000

    Update

--- .config/fish/functions/load_nvm.fish
+++ .config/fish/functions/load_nvm.fish
@@ -1,20 +1,4 @@
 function load_nvm --on-variable="PWD"
-  set -l nvmrc_path (nvm_find_nvmrc)
-  if test -n "$nvmrc_path"
-  	set -l nvmrc_version (cat $nvmrc_path)
-    set -l nvmrc_node_version (nvm version $nvmrc_version)
-    if test "$nvmrc_node_version" = "N/A"
-      nvm install $nvmrc_version
-			nvm use $nvmrc_version
-    else 
-    	if test "$nvmrc_node_version" != (nvm version)
-				nvm use $nvmrc_node_version
-			end
-    end
-  else 
-  	if test (nvm version default) != (nvm version)
-			nvm use default
-		end
-  end
+	nvm use
 end
 
