a335611e4694662f745733e438ef00f52c776eff
André de Jager <AndreTheHunter@gmail.com>
2023-03-23 10:53:35 +1000

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,5 @@ source $OMF_PATH/init.fish
 # uninstall by removing these lines or running `tabtab uninstall slss`
 [ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/slss.fish
 
-[ -s "$HOME/.jabba/jabba.fish" ]; and source "$HOME/.jabba/jabba.fish"
-
 # Created by `pipx` on 2022-09-11 23:19:28
 set PATH $PATH "$HOME/.local/bin"
