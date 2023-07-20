ac850fcd63509d0fbd99e3ef8f74167742e7be17
André de Jager <AndreTheHunter@gmail.com>
2023-07-20 15:20:52 +1000

    Update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
-{}
-;{:user {:plugins [[lein-pprint "1.3.2"]
-;                  [lein-ancient "0.7.0"] 
+{:portal {:dependencies [[djblue/portal "0.42.1"]]}
+ :user {:plugins [[lein-pprint "1.3.2"]
+                  [lein-ancient "0.7.0"]]}} 
