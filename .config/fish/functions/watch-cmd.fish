3c4fbef0e0bf05e3f1c956cead5532d5a311f7a7
André de Jager <AndreTheHunter@gmail.com>
2023-12-08 00:38:48 +1000

    Update

--- .config/fish/functions/watch-cmd.fish
+++ .config/fish/functions/watch-cmd.fish
@@ -1,3 +1,9 @@
 function watch-cmd
-	ag -l | entr -ccsn "$argv"
+	set -l file $argv[1]
+	set -l cmd $argv[2]
+	if test -z "$cmd"
+		set cmd "$file"
+		set file '.'
+	end
+	ls $file | entr -ccsn "$cmd"
 end
