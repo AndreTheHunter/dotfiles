27a7148e760f53283b3dc769958195932e65cf7b
André de Jager <AndreTheHunter@gmail.com>
2024-01-18 16:07:16 +1000

    Update

--- .config/fish/functions/cdg.fish
+++ .config/fish/functions/cdg.fish
@@ -1,6 +1,6 @@
 function cdg
-	set toplevel (command git rev-parse --show-toplevel 2>/dev/null)
-	if test -n $toplevel
+	set --function toplevel (git rev-parse --show-cdup 2>/dev/null)
+	if test $status -eq 0;and test -n $toplevel
 		cd $toplevel
 	end
 end
