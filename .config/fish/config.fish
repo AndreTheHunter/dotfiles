768182dea845dfa8746afc1e83fa55d43479020d
André de Jager <AndreTheHunter@gmail.com>
2024-07-16 17:35:55 +1000

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,2 @@
-#FIXME add https://www.iterm2.com/documentation-shell-integration.html
-
 set -gx OMF_PATH "$HOME/.local/share/omf"
 source $OMF_PATH/init.fish
--- .config/fish/functions/fish_prompt.fish
+++
@@ -1,1 +0,0 @@
-/Users/andre/.local/share/omf/themes/bobthefish/fish_prompt.fish
