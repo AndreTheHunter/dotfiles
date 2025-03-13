7c2c4a445f41b1e6c2c1c980ab72895322efc018
André de Jager <AndreTheHunter@gmail.com>
2025-03-13 17:39:40 +1000

    Update

--- .config/fish/functions/parents.fish
+++ .config/fish/functions/parents.fish
@@ -1,15 +1,16 @@
 function parents
-	set -l dir $argv
-	if test (count $dir) -eq 0
-		set dir $PWD
-	end
-	set -l path (echo $dir|tr '/' '\n')
-	while test (count $path) -gt 0
-		set -e path[-1]
-		if test (count $path) -gt 1
-			string join '/' $path
-		else if test (count $path) -eq 1
-			echo '/'
-		end
-	end
+    #FIXME use __fish_parent_directories
+    set -l dir $argv
+    if test (count $dir) -eq 0
+        set dir $PWD
+    end
+    set -l path (echo $dir|tr '/' '\n')
+    while test (count $path) -gt 0
+        set -e path[-1]
+        if test (count $path) -gt 1
+            string join / $path
+        else if test (count $path) -eq 1
+            echo /
+        end
+    end
 end
