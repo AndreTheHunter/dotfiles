4fc2682ccabe308216c35a0a4bba997d7b429c7b
André de Jager <AndreTheHunter@gmail.com>
2025-07-21 11:45:52 +1000

    Update

--- .config/fish/functions/ide.fish
+++ .config/fish/functions/ide.fish
@@ -1,21 +1,24 @@
 function ide
-	set project (basename $PWD)
-	if test -e "$project.sln"
-		set cmd 'rider'
-		set args "$project.sln"
-	else
-		set cmd 'idea'
-	end
-	if not set --query args
-		if test (count $argv) -eq 0
-			set args .
-		else
-			set args $argv
-		end
-	end
-	set_color yellow
-	echo "$cmd $args"
-	set_color normal
-	nohup $cmd $args > /dev/null &
-	disown
+    if git-is-repo
+        show cdg
+    end
+    set project (basename $PWD)
+    if test -e "$project.sln"
+        set cmd rider
+        set args "$project.sln"
+    else
+        set cmd idea
+    end
+    if not set --query args
+        if test (count $argv) -eq 0
+            set args .
+        else
+            set args $argv
+        end
+    end
+    set_color yellow
+    echo "$cmd $args"
+    set_color normal
+    nohup $cmd $args >/dev/null &
+    disown
 end
