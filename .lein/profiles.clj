c706756604bdc25e4ccbe5ec5cb7a6f29d1784b8
André de Jager <AndreTheHunter@gmail.com>
2026-04-23 10:29:16 +1000

    Update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,8 @@
 {:portal {:dependencies [[djblue/portal "0.59.1"]]}
- :user {:plugins [[lein-pprint "1.3.2"]
-                  [lein-ancient "0.7.0"]]}} 
+ :user {:plugins  [[lein-pprint "1.3.2"]
+                   [lein-ancient "0.7.0"]]
+         :jvm-opts ["-Daether.dependencyCollector.impl=bf"
+                    "-Daether.dependencyCollector.bf.threads=64"
+                    "-Daether.connector.basic.threads=64"
+                    "-Dmaven.wagon.http.pool.maxPerRoute=64"
+                    "-Dmaven.wagon.http.pool.maxTotal=64"]}} 
